4e7315edd99bb60e03e768e3091b3f04bd9abe75
apoorv-neosofttech <apoorv.rane@neosoftmail.com>
2022-01-13 20:18:21 +0530

    Assignment 2

---
+++ HiddenVilla_Server/Model/BlazorRoom.cs
@@ -0,0 +1,11 @@
+namespace HiddenVilla_Server.Model
+{
+  public class BlazorRoom
+  {
+    public int Id { get; set; }
+    public string? RoomName { get; set; }
+    public double Price { get; set; }
+    public bool IsActive { get; set; }
+    public List<BlazorRoomProp>? RoomProps { get; set; }
+  }
+}
---
+++ HiddenVilla_Server/Model/BlazorRoomProp.cs
@@ -0,0 +1,9 @@
+namespace HiddenVilla_Server.Model
+{
+    public class BlazorRoomProp
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Value { get; set; }
+    }
+}
