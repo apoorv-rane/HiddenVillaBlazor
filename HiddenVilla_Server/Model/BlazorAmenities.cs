6517e9b3cddb704f3348f9aa3785299420babf7c
apoorv-neosofttech <apoorv.rane@neosoftmail.com>
2022-01-15 20:22:57 +0530

    Lifecycle done

---
+++ HiddenVilla_Server/Model/BlazorAmenities.cs
@@ -0,0 +1,9 @@
+namespace HiddenVilla_Server.Model
+{
+  public class BlazorAmenities
+  {
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+  }
+}
